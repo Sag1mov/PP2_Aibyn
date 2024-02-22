1db44829a9800d21d91852761899c9bbfebb0c3c
Sagimov Aibyn <aibyn.sagimov@gmail.com>
2024-02-23 00:24:03 +0600

    regex

---
+++ tsis5/regex3.py
@@ -0,0 +1,10 @@
+import re
+
+string1 = "This is_a siuuu_text with_aibo sequences_of_lowercase_letters_joined_with_underscores."
+
+
+sequence = re.findall(r'\b[a-z]+(?:_[a-z]+)+\b', string1)
+
+print("Нашлось:")
+for x in sequence:
+    print(x)
