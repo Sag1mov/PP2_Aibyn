23ac324764759c0dcfafe6582e4506f0549c4463
Sagimov Aibyn <aibyn.sagimov@gmail.com>
2024-04-08 00:06:17 +0500

    TSIS9

---
+++ TSIS8/snake.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import random
+ 
+pygame.init()
+ 
+# ЦВЕТА
+yellow = (255, 255, 102)
+red = (213, 50, 80)
+blue = (0, 0, 255)
+green = (0,255,0)
+white = (255, 255, 255)
+black = (0, 0, 0)
